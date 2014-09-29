6869f0819b133737fe735a4201ef3e049d1b8b57
Bernd Vogt <bernd.vogt@sourcepit.org>
2014-09-29 14:00:55 +0200

    #1: Apache License Version 2.0

--- maven-bootstrap-core/src/main/java/org/sourcepit/maven/bootstrap/internal/core/ReactorReader.java
+++ maven-bootstrap-core/src/main/java/org/sourcepit/maven/bootstrap/internal/core/ReactorReader.java
@@ -1,20 +1,36 @@
+/*
+ * Copyright 2014 Bernd Vogt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.sourcepit.maven.bootstrap.internal.core;
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -39,218 +55,217 @@ import java.util.Map;
  * 
  * @author Jason van Zyl
  */
-public class ReactorReader
-    implements WorkspaceReader
+public class ReactorReader implements WorkspaceReader
 {
 
-    private Map<String, MavenProject> projectsByGAV;
+   private Map<String, MavenProject> projectsByGAV;
 
-    private Map<String, List<MavenProject>> projectsByGA;
+   private Map<String, List<MavenProject>> projectsByGA;
 
-    private WorkspaceRepository repository;
+   private WorkspaceRepository repository;
 
-    public ReactorReader( Map<String, MavenProject> reactorProjects )
-    {
-        projectsByGAV = reactorProjects;
+   public ReactorReader(Map<String, MavenProject> reactorProjects)
+   {
+      projectsByGAV = reactorProjects;
 
-        projectsByGA = new HashMap<String, List<MavenProject>>( reactorProjects.size() * 2 );
-        for ( MavenProject project : reactorProjects.values() )
-        {
-            String key = ArtifactUtils.versionlessKey( project.getGroupId(), project.getArtifactId() );
+      projectsByGA = new HashMap<String, List<MavenProject>>(reactorProjects.size() * 2);
+      for (MavenProject project : reactorProjects.values())
+      {
+         String key = ArtifactUtils.versionlessKey(project.getGroupId(), project.getArtifactId());
 
-            List<MavenProject> projects = projectsByGA.get( key );
+         List<MavenProject> projects = projectsByGA.get(key);
 
-            if ( projects == null )
+         if (projects == null)
+         {
+            projects = new ArrayList<MavenProject>(1);
+            projectsByGA.put(key, projects);
+         }
+
+         projects.add(project);
+      }
+
+      repository = new WorkspaceRepository("reactor", new HashSet<String>(projectsByGAV.keySet()));
+   }
+
+   private File find(MavenProject project, Artifact artifact)
+   {
+      if ("pom".equals(artifact.getExtension()))
+      {
+         return project.getFile();
+      }
+
+      org.apache.maven.artifact.Artifact projectArtifact = findMatchingArtifact(project, artifact);
+
+      if (hasArtifactFileFromPackagePhase(projectArtifact))
+      {
+         return projectArtifact.getFile();
+      }
+      else if (!hasBeenPackaged(project))
+      {
+         // fallback to loose class files only if artifacts haven't been packaged yet
+
+         if (isTestArtifact(artifact))
+         {
+            if (project.hasLifecyclePhase("test-compile"))
             {
-                projects = new ArrayList<MavenProject>( 1 );
-                projectsByGA.put( key, projects );
+               return new File(project.getBuild().getTestOutputDirectory());
             }
-
-            projects.add( project );
-        }
-
-        repository = new WorkspaceRepository( "reactor", new HashSet<String>( projectsByGAV.keySet() ) );
-    }
-
-    private File find( MavenProject project, Artifact artifact )
-    {
-        if ( "pom".equals( artifact.getExtension() ) )
-        {
-            return project.getFile();
-        }
-
-        org.apache.maven.artifact.Artifact projectArtifact = findMatchingArtifact( project, artifact );
-
-        if ( hasArtifactFileFromPackagePhase( projectArtifact ) )
-        {
-            return projectArtifact.getFile();
-        }
-        else if ( !hasBeenPackaged( project ) )
-        {
-            // fallback to loose class files only if artifacts haven't been packaged yet
-
-            if ( isTestArtifact( artifact ) )
+         }
+         else
+         {
+            if (project.hasLifecyclePhase("compile"))
             {
-                if ( project.hasLifecyclePhase( "test-compile" ) )
-                {
-                    return new File( project.getBuild().getTestOutputDirectory() );
-                }
+               return new File(project.getBuild().getOutputDirectory());
             }
-            else
+         }
+      }
+
+      // The fall-through indicates that the artifact cannot be found;
+      // for instance if package produced nothing or classifier problems.
+      return null;
+   }
+
+   private boolean hasArtifactFileFromPackagePhase(org.apache.maven.artifact.Artifact projectArtifact)
+   {
+      return projectArtifact != null && projectArtifact.getFile() != null && projectArtifact.getFile().exists();
+   }
+
+   private boolean hasBeenPackaged(MavenProject project)
+   {
+      return project.hasLifecyclePhase("package") || project.hasLifecyclePhase("install")
+         || project.hasLifecyclePhase("deploy");
+   }
+
+   /**
+    * Tries to resolve the specified artifact from the artifacts of the given project.
+    * 
+    * @param project The project to try to resolve the artifact from, must not be <code>null</code>.
+    * @param requestedArtifact The artifact to resolve, must not be <code>null</code>.
+    * @return The matching artifact from the project or <code>null</code> if not found.
+    */
+   private org.apache.maven.artifact.Artifact findMatchingArtifact(MavenProject project, Artifact requestedArtifact)
+   {
+      String requestedRepositoryConflictId = getConflictId(requestedArtifact);
+
+      org.apache.maven.artifact.Artifact mainArtifact = project.getArtifact();
+      if (requestedRepositoryConflictId.equals(getConflictId(mainArtifact)))
+      {
+         return mainArtifact;
+      }
+
+      Collection<org.apache.maven.artifact.Artifact> attachedArtifacts = project.getAttachedArtifacts();
+      if (attachedArtifacts != null && !attachedArtifacts.isEmpty())
+      {
+         for (org.apache.maven.artifact.Artifact attachedArtifact : attachedArtifacts)
+         {
+            if (requestedRepositoryConflictId.equals(getConflictId(attachedArtifact)))
             {
-                if ( project.hasLifecyclePhase( "compile" ) )
-                {
-                    return new File( project.getBuild().getOutputDirectory() );
-                }
+               return attachedArtifact;
             }
-        }
+         }
+      }
 
-        // The fall-through indicates that the artifact cannot be found;
-        // for instance if package produced nothing or classifier problems.
-        return null;
-    }
+      return null;
+   }
 
-    private boolean hasArtifactFileFromPackagePhase( org.apache.maven.artifact.Artifact projectArtifact )
-    {
-        return projectArtifact != null && projectArtifact.getFile() != null && projectArtifact.getFile().exists();
-    }
+   /**
+    * Gets the repository conflict id of the specified artifact. Unlike the dependency conflict id, the repository
+    * conflict id uses the artifact file extension instead of the artifact type. Hence, the repository conflict id more
+    * closely reflects the identity of artifacts as perceived by a repository.
+    * 
+    * @param artifact The artifact, must not be <code>null</code>.
+    * @return The repository conflict id, never <code>null</code>.
+    */
+   private String getConflictId(org.apache.maven.artifact.Artifact artifact)
+   {
+      StringBuilder buffer = new StringBuilder(128);
+      buffer.append(artifact.getGroupId());
+      buffer.append(':').append(artifact.getArtifactId());
+      if (artifact.getArtifactHandler() != null)
+      {
+         buffer.append(':').append(artifact.getArtifactHandler().getExtension());
+      }
+      else
+      {
+         buffer.append(':').append(artifact.getType());
+      }
+      if (artifact.hasClassifier())
+      {
+         buffer.append(':').append(artifact.getClassifier());
+      }
+      return buffer.toString();
+   }
 
-    private boolean hasBeenPackaged( MavenProject project )
-    {
-        return project.hasLifecyclePhase( "package" ) || project.hasLifecyclePhase( "install" )
-            || project.hasLifecyclePhase( "deploy" );
-    }
+   private String getConflictId(Artifact artifact)
+   {
+      StringBuilder buffer = new StringBuilder(128);
+      buffer.append(artifact.getGroupId());
+      buffer.append(':').append(artifact.getArtifactId());
+      buffer.append(':').append(artifact.getExtension());
+      if (artifact.getClassifier().length() > 0)
+      {
+         buffer.append(':').append(artifact.getClassifier());
+      }
+      return buffer.toString();
+   }
 
-    /**
-     * Tries to resolve the specified artifact from the artifacts of the given project.
-     * 
-     * @param project The project to try to resolve the artifact from, must not be <code>null</code>.
-     * @param requestedArtifact The artifact to resolve, must not be <code>null</code>.
-     * @return The matching artifact from the project or <code>null</code> if not found.
-     */
-    private org.apache.maven.artifact.Artifact findMatchingArtifact( MavenProject project, Artifact requestedArtifact )
-    {
-        String requestedRepositoryConflictId = getConflictId( requestedArtifact );
+   /**
+    * Determines whether the specified artifact refers to test classes.
+    * 
+    * @param artifact The artifact to check, must not be {@code null}.
+    * @return {@code true} if the artifact refers to test classes, {@code false} otherwise.
+    */
+   private static boolean isTestArtifact(Artifact artifact)
+   {
+      return ("test-jar".equals(artifact.getProperty("type", "")))
+         || ("jar".equals(artifact.getExtension()) && "tests".equals(artifact.getClassifier()));
+   }
 
-        org.apache.maven.artifact.Artifact mainArtifact = project.getArtifact();
-        if ( requestedRepositoryConflictId.equals( getConflictId( mainArtifact ) ) )
-        {
-            return mainArtifact;
-        }
+   public File findArtifact(Artifact artifact)
+   {
+      String projectKey = ArtifactUtils.key(artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion());
 
-        Collection<org.apache.maven.artifact.Artifact> attachedArtifacts = project.getAttachedArtifacts();
-        if ( attachedArtifacts != null && !attachedArtifacts.isEmpty() )
-        {
-            for ( org.apache.maven.artifact.Artifact attachedArtifact : attachedArtifacts )
-            {
-                if ( requestedRepositoryConflictId.equals( getConflictId( attachedArtifact ) ) )
-                {
-                    return attachedArtifact;
-                }
-            }
-        }
+      MavenProject project = projectsByGAV.get(projectKey);
 
-        return null;
-    }
+      if (project != null)
+      {
+         File file = find(project, artifact);
+         if (file == null && project != project.getExecutionProject())
+         {
+            file = find(project.getExecutionProject(), artifact);
+         }
+         return file;
+      }
 
-    /**
-     * Gets the repository conflict id of the specified artifact. Unlike the dependency conflict id, the repository
-     * conflict id uses the artifact file extension instead of the artifact type. Hence, the repository conflict id more
-     * closely reflects the identity of artifacts as perceived by a repository.
-     * 
-     * @param artifact The artifact, must not be <code>null</code>.
-     * @return The repository conflict id, never <code>null</code>.
-     */
-    private String getConflictId( org.apache.maven.artifact.Artifact artifact )
-    {
-        StringBuilder buffer = new StringBuilder( 128 );
-        buffer.append( artifact.getGroupId() );
-        buffer.append( ':' ).append( artifact.getArtifactId() );
-        if ( artifact.getArtifactHandler() != null )
-        {
-            buffer.append( ':' ).append( artifact.getArtifactHandler().getExtension() );
-        }
-        else
-        {
-            buffer.append( ':' ).append( artifact.getType() );
-        }
-        if ( artifact.hasClassifier() )
-        {
-            buffer.append( ':' ).append( artifact.getClassifier() );
-        }
-        return buffer.toString();
-    }
+      return null;
+   }
 
-    private String getConflictId( Artifact artifact )
-    {
-        StringBuilder buffer = new StringBuilder( 128 );
-        buffer.append( artifact.getGroupId() );
-        buffer.append( ':' ).append( artifact.getArtifactId() );
-        buffer.append( ':' ).append( artifact.getExtension() );
-        if ( artifact.getClassifier().length() > 0 )
-        {
-            buffer.append( ':' ).append( artifact.getClassifier() );
-        }
-        return buffer.toString();
-    }
+   public List<String> findVersions(Artifact artifact)
+   {
+      String key = ArtifactUtils.versionlessKey(artifact.getGroupId(), artifact.getArtifactId());
 
-    /**
-     * Determines whether the specified artifact refers to test classes.
-     * 
-     * @param artifact The artifact to check, must not be {@code null}.
-     * @return {@code true} if the artifact refers to test classes, {@code false} otherwise.
-     */
-    private static boolean isTestArtifact( Artifact artifact )
-    {
-        return ( "test-jar".equals( artifact.getProperty( "type", "" ) ) )
-            || ( "jar".equals( artifact.getExtension() ) && "tests".equals( artifact.getClassifier() ) );
-    }
+      List<MavenProject> projects = projectsByGA.get(key);
+      if (projects == null || projects.isEmpty())
+      {
+         return Collections.emptyList();
+      }
 
-    public File findArtifact( Artifact artifact )
-    {
-        String projectKey = ArtifactUtils.key( artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion() );
+      List<String> versions = new ArrayList<String>();
 
-        MavenProject project = projectsByGAV.get( projectKey );
+      for (MavenProject project : projects)
+      {
+         if (find(project, artifact) != null)
+         {
+            versions.add(project.getVersion());
+         }
+      }
 
-        if ( project != null )
-        {
-            File file = find( project, artifact );
-            if ( file == null && project != project.getExecutionProject() )
-            {
-                file = find( project.getExecutionProject(), artifact );
-            }
-            return file;
-        }
+      return Collections.unmodifiableList(versions);
+   }
 
-        return null;
-    }
-
-    public List<String> findVersions( Artifact artifact )
-    {
-        String key = ArtifactUtils.versionlessKey( artifact.getGroupId(), artifact.getArtifactId() );
-
-        List<MavenProject> projects = projectsByGA.get( key );
-        if ( projects == null || projects.isEmpty() )
-        {
-            return Collections.emptyList();
-        }
-
-        List<String> versions = new ArrayList<String>();
-
-        for ( MavenProject project : projects )
-        {
-            if ( find( project, artifact ) != null )
-            {
-                versions.add( project.getVersion() );
-            }
-        }
-
-        return Collections.unmodifiableList( versions );
-    }
-
-    public WorkspaceRepository getRepository()
-    {
-        return repository;
-    }
+   public WorkspaceRepository getRepository()
+   {
+      return repository;
+   }
 
 }
